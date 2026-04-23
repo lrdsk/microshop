3d7dfe01a5985e681a087f76ca44ba17951b8dc4
yura <yura_alex2004@mail.ru>
2026-04-23 14:20:29 +0300

    feat: Изменить сообщение в логе

--- order_service/src/main/java/com/example/microshop/order_service/service/user/UserServiceImpl.java
+++ order_service/src/main/java/com/example/microshop/order_service/service/user/UserServiceImpl.java
@@ -34,7 +34,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void register(RegisterUserCommand command) {
-        log.info("Called UserService to register new user with username: {} and email: {}", command.username(), command.email());
+        log.info("Called UserService to register new user with username: {}", command.username());
         Optional<UserEntity> userEntity = userRepository.findByUsername(command.username());
 
         if(userEntity.isPresent()) {
